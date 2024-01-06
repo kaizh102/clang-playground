062c783f3bcf712364cfd74742f3b47f213abb1a
Kai Zhang <kai.zh102@outlook.com>
2024-01-06 17:06:05 +0800

    cleanup

--- source/clang-playground.c
+++ source/clang-playground.c
@@ -2,11 +2,7 @@
 #include <stdbool.h>
 #include "lib.h"
 
-#include <ncurses.h>
-
 int main(void) {
-
-
     printf("externally defined variable a = %d\n", a);
     bool lf = true;
     bool ri = true;
@@ -16,19 +12,5 @@ int main(void) {
     printf("lf?1:0 = %d\n", (lf?1:0));
     printf("lf+ri+(lf?1:0) = %d\n", lf+ri+(lf?1:0));
     printf("lf+ri+lf?1:0 = %d\n", lf+ri+lf?1:0);
-
-    initscr();  // ncurses function
-    raw();
-    keypad(stdscr, TRUE);
-
-    int ch;
-    while ((ch = getch()) != KEY_F(2)) {
-        printw("Key pressed: %d\n", ch);
-        refresh();
-    }
-
-    endwin();
-
-
     return 0;
 }
